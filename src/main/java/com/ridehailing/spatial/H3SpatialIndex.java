0f57eee07298de4fec1b89ee5a4cb1e6ff7fdbd4
Abhiram Tarun Geddam <abhigeddam94@gmail.com>
2026-08-29 17:30:27 +0530

    initialize H3SpatialIndex wrapper for Uber H3 library

---
+++ src/main/java/com/ridehailing/spatial/H3SpatialIndex.java
@@ -0,0 +1,19 @@
+package com.ridehailing.spatial;
+
+import com.uber.h3core.H3Core;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+
+public class H3SpatialIndex implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public static final int DEFAULT_RESOLUTION = 8; // ~460m edge length
+
+    private static transient H3Core h3;
+
+    public static synchronized H3Core getH3Instance() {
+        if (h3 == null) {
+            try {
+                h3 = H3Core.newInstance();
+            } catch (IOException e) {
+}
